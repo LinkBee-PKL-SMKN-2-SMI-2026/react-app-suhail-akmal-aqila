72843f68cd40952d1f534ad0440ea0e0e1aa0294
kinmintaka123 <kinmintaka@gmail.com>
2026-08-24 01:23:29 +0700

    feat: menghapus kodingan

--- src/types/Vehicle.ts
+++ src/types/Vehicle.ts
@@ -1,34 +1,3 @@
-
-// export interface VehicleFormData {
-//     name: string;
-//     brand: string;
-//     plateNumber: string;
-//     transmission: string;
-//     category: string;
-// }
-
-// export interface Vehicle {
-//     id: string;
-//     name: string;
-//     brand: string;
-//     plateNumber: string;
-//     transmission: string;
-//     category: {
-//         name: string;
-//     };
-// }
-
-// export interface VehicleProps {
-//     id: string | number;
-//     name: string;
-//     brand: string;
-//     plateNumber: string;
-//     transmission: string;
-//     category?: {
-//         name: string;
-//     };
-// }
-
 export interface Vehicle {
   id: string;
   name: string;
